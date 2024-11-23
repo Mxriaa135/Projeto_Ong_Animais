49afe20e92af30602eee4da88b8e615b9fbcc4d0
cristanes <cristane123@gmail.com>
2024-11-23 17:42:04 -0300

    inserção das tuplas

--- MySql/DML.sql
+++ MySql/DML.sql
@@ -12,7 +12,10 @@ INSERT INTO usuario(CPF, nome, sobrenome, email, senha, sexo, data_nascimento, t
 ('48783632105', 'Ana Lívia', 'Marques Wermelinger', 'amarques123@gmail.com', '1234ana', 'F', '2000-10-18','11973451234'),
 ('43591864307', 'Paulo', 'de Araujo Valverde', 'paulinho1994@gmail.com', '1bf245','M', '1994-08-07','11945634567'),
 ('35748693883', 'Sandra', 'de Jesus', 'jesus@hotmail.com', 'eu345ue', 'F', '1984-05-12', '11957756830'),
-('36709925691','Giovanne', 'Prado Brito', 'pradobg@gmail.com', 'Gpr4d0', 'M', '1980-12-22', '11957899600');
+('36709925691','Giovanne', 'Prado Brito', 'pradobg@gmail.com', 'Gpr4d0', 'M', '1980-12-22', '11957899600'),
+('58963578818', 'João' 'Silva', 'joao.silva@email.com', 'jos1lva', 'M', '1997-04-03'  '11987654321'),
+('58963578818', 'Maria', 'Souza', 'maria.souza@email.com', 'msouza321', 'F', '1995-04-04', '21987654321');
+
 -- ('', 'Nicoly', '', ''),
 -- ('', 'Cristane', ''),
 -- ('', 'Gabriel', ''), 
@@ -32,13 +35,18 @@ INSERT INTO usuario_tipo (id_TipoUsuario, CPF_usuario) VALUES
 ('6','35748693883'),
 ('3', '48783632105'),
 ('6', '43591864307'),
-('6', '36709925691');
+('6', '36709925691'),
+('3', '53328174916'),
+('1', ' 58963578818');
 -- ('','')...
 
 
 INSERT INTO animal (nome, idade, sexo, peso, especie, descricao) VALUES 
 ('Pipoca', 14, 'M', 5.500, 'cachorro', 'Muito brincalhão'),
-('Lilica', 20, 'F', 5.289, 'gato', 'Muito carinhosa e brincalhona');
+('Lilica', 20, 'F', 5.289, 'gato', 'Muito carinhosa e brincalhona'),
+('Chanel', 24, 'F', 5.400, 'cachorro', 'Muito companheira'),
+('Snoopy', 20, 'M', 5.500, 'cachorro', 'Muito dócil e carinhoso' );
+
 
 INSERT INTO animal (nome, idade, sexo, peso, especie, descricao, deficiencia, doenca) VALUES
 ('Torresmo', 84, 'M', 20.399, 'cachorro', 'Dócil e brincalhão, adora cosquinha na barriga', 'cego', 'renal cronico'),
@@ -50,24 +58,32 @@ INSERT INTO adocao (id_animal, CPF_usuario) VALUES
 ('1', '83274873465'),
 ('2', '52541088846'),
 ('3', '43591864307'),
-('4', '35748693883');
+('4', '35748693883'),
+('5', '53328174916'),
+('6', '58963578818');
 
 INSERT INTO doacao_item (nome_item, foto, CPF_usuario ) VALUES 
 ('Ração', '', '88728982984'),
 ('Shampoo para cachorros', '', '52541088846'),
 ('Ração', '', '48783632105'),
-('Roupa para cachorro', '', '48783632105');
+('Roupa para cachorro', '', '48783632105'),
+('Toalha', '', '58963578818 '),
+('Caminha', '', '53328174916');
 
 INSERT INTO Contribuicao_financeira (valor, CPF_usuario, id_metodoPagamento) VALUES
 (5.00, '52541088846', 1),
 (10.00, '88728982984', 1),
 (30.00, '36709925691',1),
-(50.00, '43591864307',1);
+(50.00, '43591864307',1),
+('40.00,', '53328174916', '1'),
+('20.00','58963578818', '1');
 
 INSERT INTO Contribuicao_recorrente (valor_fixo, frequencia, CPF_usuario, id_metodoPagamento) VALUES
 ('30.00', 'Mensalmente', '52541088846', 2),
 ('20.00', 'Trimestral', '83274873465', 2),
 ('50.00', 'Mensalmente',  '36709925691',3),
-('150.00', 'Trimestral', '43591864307', 2)
+('150.00', 'Trimestral', '43591864307', 2),
+('80.00','Mensalmente', '58963578818', '1'),
+('20.00,', 'Mensalmente', '53328174916', '1')
 ;
 
