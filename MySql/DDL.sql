57059d009b3816cb7c9eaa03c97585b9bf2a2e60
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2024-11-24 22:35:44 -0300

    Alterações nos tipos e tamanhos dos atributos

--- MySql/DDL.sql
+++ MySql/DDL.sql
@@ -2,7 +2,7 @@ CREATE DATABASE ong_database;
 USE ong_database;
 CREATE TABLE Animal(
     id_animal INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(15) NOT NULL,
+    nome VARCHAR(20) NOT NULL,
     idade INT CHECK(idade>2 AND idade<=240),
     sexo CHAR(1) NOT NULL CHECK(sexo="F" OR sexo="M"),
     peso FLOAT NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE Usuario (
 	CPF CHAR(11) PRIMARY KEY,
 	nome VARCHAR(15) NOT NULL,
 	sobrenome VARCHAR (40) NOT NULL,
-	email VARCHAR(50) NOT NULL UNIQUE,
+	email VARCHAR(60) NOT NULL UNIQUE,
 	senha VARCHAR(20) NOT NULL,
 	sexo CHAR (1) NOT NULL CHECK(sexo="F" OR sexo="M" OR sexo="I"),
 	data_nascimento DATE NOT NULL,
@@ -28,13 +28,13 @@ CREATE TABLE Usuario (
 );
 
 CREATE TABLE tipo_usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id SMALLINT PRIMARY KEY AUTO_INCREMENT,
 	descricao VARCHAR(15) NOT NULL UNIQUE CHECK(descricao='Administrador' OR descricao='Usuario' OR
 	descricao='Voluntário' OR descricao='Doador' OR descricao='Adotante' OR descricao='Contribuinte')
 );
 
 CREATE TABLE usuario_tipo(
-	id_TipoUsuario INT,
+	id_TipoUsuario SMALLINT,
 	CPF_usuario CHAR(11) NOT NULL,
 	FOREIGN KEY(id_TipoUsuario) REFERENCES tipo_usuario (id),
 	FOREIGN KEY (CPF_Usuario) REFERENCES Usuario (CPF),
@@ -55,7 +55,7 @@ CREATE TABLE Adocao (
 
 CREATE TABLE  Doacao_Item (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome_item VARCHAR (30),
+	nome_item VARCHAR (40),
 	foto VARCHAR(225) NOT NULL,
 	status_doacao CHAR(1) NULL CHECK(status_doacao='P' OR status_doacao='A' OR status_doacao='R')  DEFAULT 'P', 
 	observacao VARCHAR(100) NULL,
@@ -66,29 +66,29 @@ CREATE TABLE  Doacao_Item (
 );
 
 CREATE TABLE Metodo_pagamento (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id SMALLINT PRIMARY KEY AUTO_INCREMENT, -- ATENCAO tipo smallint
 	descricao VARCHAR(25) NOT NULL UNIQUE CHECK(descricao='PIX' OR
     descricao='Cartão de Crédito' OR descricao='Cartão de Débito' OR descricao='Boleto bancário')
 );
 
 CREATE TABLE Contribuicao_financeira(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	valor FLOAT NOT NULL CHECK(valor>0),
+	valor FLOAT NOT NULL CHECK(valor>2),
 	observacao VARCHAR(100) NULL,
 	data_contribuicao DATE DEFAULT (CURDATE()),
 	CPF_usuario CHAR(11) NOT NULL,
-	id_metodoPagamento INT NOT NULL,
+	id_metodoPagamento SMALLINT NOT NULL,
 	FOREIGN KEY (CPF_usuario) REFERENCES Usuario(CPF),
 	FOREIGN KEY (id_metodoPagamento) REFERENCES Metodo_pagamento (id)
 );
 
 CREATE TABLE Contribuicao_recorrente(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	valor_fixo FLOAT NOT NULL CHECK(valor_fixo>0),
+	valor_fixo FLOAT NOT NULL CHECK(valor_fixo>2), 
 	data_cobranca DATE DEFAULT (CURDATE()),
 	frequencia VARCHAR(20) NOT NULL,
 	CPF_usuario CHAR(11) NOT NULL,
-	id_metodoPagamento INT NOT NULL,
+	id_metodoPagamento SMALLINT NOT NULL,
 	FOREIGN KEY (CPF_usuario) REFERENCES Usuario(CPF),
 	FOREIGN KEY (id_metodoPagamento) REFERENCES Metodo_pagamento(id)
 );
