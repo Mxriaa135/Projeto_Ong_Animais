82d5fe501b7d4635406caa86c1bada7fc6b6f347
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2024-11-29 09:27:27 -0300

    Modificação no DTL

--- MySql/DTL.sql
+++ MySql/DTL.sql
@@ -1,10 +1,12 @@
 DELIMITER //
 START TRANSACTION;
 
+UPDATE adocao SET status_adocao = 'A' WHERE id_animal = 2; 
 UPDATE animal SET ativo = 0 WHERE id_animal = 2;
+UPDATE adocao SET data_adocao = CURDATE() WHERE id_animal = 2;
 
-IF (SELECT status_adocao FROM adocao WHERE id_animal = 2) = 'A' 
-    COMMIT
+IF (SELECT status_adocao FROM adocao WHERE id_animal = 2) = 'P' 
+    COMMIT;
 ELSE
 	ROLLBACK;
 END IF;
